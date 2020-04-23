7d2022df4f64d73d28487fa4499abf4b7bc34e90
db <41982552+deathbask3t@users.noreply.github.com>
2020-04-23 16:13:17 -0700

    Update cfg.js

--- fe/src/services/cfg.js
+++ fe/src/services/cfg.js
@@ -4,6 +4,16 @@ import * as cfg from '../constants/view'
 // keep a reference to use as a singleton
 var obj
 
+const viewWidth = 600
+const chartLeftXOffset = 60
+const chartRightXOffset = 60
+const chartWidth = viewWidth - (chartLeftXOffset + chartRightXOffset)
+const rectCount = 40
+const rectAndSpacingWidth = chartWidth / rectCount
+
+const rectSpace = 1
+const rectWidth = rectAndSpacingWidth - rectSpace
+
 function Cfg() {
   obj = obj || {
     period: cfg.PERIOD_DAILY,
@@ -13,18 +23,21 @@ function Cfg() {
     ticker: 'RMP',
     showLine: true,
     chart: {
-      viewWidth: 600,
+      viewWidth,
       viewHeight: 300,
-      chartXOffset: 60,
+      chartLeftXOffset,
+      charRightXOffset,
+      chartWidth,
       chartYOffset: 50,
       chartHeight: 300,
       chartXQuartile: 96,
       chartYQuartile: 75,
       chartYAxisLabels: [0, 25, 50, 75, 100],
       chartXAxisLabels: [0, 10, 20, 30, 40],
-      rectWidth: 11,
-      rectAndSpacingWidth: 12,
-      rectCount: 40,
+      rectWidth,
+      rectSpace,
+      rectCount,
+      rectAndSpacingWidth,
       candleWidth: 5,
       candleOffset: 3.5,
     }
