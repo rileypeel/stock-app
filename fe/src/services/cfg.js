31f89a4531ee347f0b8728fda0aa04a0dbd9e173
Riley Peel <rpeel4@outlook.com>
2020-05-06 20:47:18 -0700

    fixed merge conflicts

--- fe/src/services/cfg.js
+++ fe/src/services/cfg.js
@@ -5,19 +5,11 @@ import * as cfg from '../constants/view'
 // keep a reference to use as a singleton
 var obj
 
-<<<<<<< HEAD
-const viewWidth = 600
-const chartLeftXOffset = 60
-const chartRightXOffset = 60
-const chartWidth = viewWidth - (chartLeftXOffset + chartRightXOffset)
-const rectCount = 40
-=======
 const chartXOffset = 20
 const viewWidth = 600
 const viewHeight = 300
 const chartWidth = viewWidth - (chartXOffset * 2)
 const rectCount = 200
->>>>>>> bellsandwhistles
 const rectAndSpacingWidth = chartWidth / rectCount
 
 const rectSpace = 1
@@ -36,33 +28,19 @@ function Cfg() {
     showLine: true,
     chart: {
       viewWidth,
-<<<<<<< HEAD
-      viewHeight: 300,
-      chartLeftXOffset,
-      charRightXOffset,
-      chartWidth,
-=======
       viewHeight,
       chartWidth,
       chartXOffset,
->>>>>>> bellsandwhistles
       chartYOffset: 50,
       chartHeight: viewHeight,
       chartXQuartile: 93,
       chartYQuartile: 75,
       chartYAxisLabels: [0, 25, 50, 75, 100],
       chartXAxisLabels: [0, 10, 20, 30, 40],
-<<<<<<< HEAD
-      rectWidth,
-      rectSpace,
-      rectCount,
-      rectAndSpacingWidth,
-=======
       chartXAxisBottomLabels: [0, 10, 20, 30, 40],
       rectWidth,
       rectAndSpacingWidth,
       rectCount,
->>>>>>> bellsandwhistles
       candleWidth: 5,
       candleOffset: 3.5,
       fontSize: '12px'
@@ -73,7 +51,3 @@ function Cfg() {
 }
 
 export default Cfg
-<<<<<<< HEAD
-  
-=======
->>>>>>> bellsandwhistles
