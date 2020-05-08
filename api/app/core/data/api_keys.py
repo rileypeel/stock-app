6e907b9dac629010754dd08d631c7142870c18f7
Riley Peel <rpeel4@outlook.com>
2020-05-08 13:19:07 -0700

    committing api keys file

---
+++ api/app/core/data/api_keys.py
@@ -0,0 +1,12 @@
+import os
+
+ALPHA_VANTAGE_KEY = os.environ.get('AV_KEY', default="key")
+
+TIINGO_KEY = os.environ.get('TIINGO_KEY', default="key")
+
+WORLD_TRADING_DATA_KEY = os.environ.get('WTD_KEY', default="key")
+
+FINNHUB_KEY = os.environ.get('FINNHUB_KEY', default="key")
+
+SIMFIN_KEY = os.environ.get('SIMFIN_KEY', default="key")
+
