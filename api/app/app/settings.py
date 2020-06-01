bca312103638c877bef0d59b9fcc7e97e434a7ea
Riley Peel <rpeel4@outlook.com>
2020-06-01 14:34:44 -0700

    another try

--- api/app/app/settings.py
+++ api/app/app/settings.py
@@ -96,10 +96,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD')
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'NAME': os.environ.get('DB_NAME', 'TESTNAME'),
+        'USER': os.environ.get('DB_USER', 'TESTUSER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'TESTPASS')
     }
 }
 
