3f799a51524085956f3322a348160b4a8489a7a0
Riley Peel <rpeel4@outlook.com>
2020-06-01 14:07:05 -0700

    set debug to true as default

--- api/app/app/settings.py
+++ api/app/app/settings.py
@@ -28,7 +28,7 @@ if not SECRET_KEY:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '3.23.61.95']
 
